042145695d91766d6c5432026d9117e29628c231
Wyatt Kirby <wyatt@apsis.io>
2019-11-22 17:53:24 -0800

    Initial commit

---
+++ lib/CFB.rb
@@ -0,0 +1,6 @@
+require "CFB/version"
+
+module CFB
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/CFB_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CFB do
+  it "has a version number" do
+    expect(CFB::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
